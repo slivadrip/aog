ce28567026dd9f9f05e70b8ff29f080c521d8816
Adriano Silva <slivadrip@gmail.com>
2019-11-20 10:49:42 -0300

    update mask money

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:aog/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,6 +19,8 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +28,19 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
+          TextFormField(
+            controller: _gasCtrl,
+            keyboardType: TextInputType.number,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 45,
+              fontFamily: "Big Shoulders Display",
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+            )
+          ),
+          
         ],
       ),
     );
